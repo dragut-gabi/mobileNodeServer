e86f59c9b17deebe360c6bb89ee761e9cb18fc01
dgir2505 <dgir2505@scs.ubbcluj.ro>
2020-11-18 02:29:10 +0200

    book

--- src/book/store.js
+++ src/book/store.js
@@ -1,34 +1,34 @@
-import dataStore from 'nedb-promise';
+import dataStore from 'nedb-promise'
 
 export class BookStore {
   constructor({ filename, autoload }) {
-    this.store = dataStore({ filename, autoload });
+    this.store = dataStore({ filename, autoload })
   }
 
   async find(props) {
-    return this.store.find(props);
+    return this.store.find(props)
   }
 
   async findOne(props) {
-    return this.store.findOne(props);
+    return this.store.findOne(props)
   }
 
   async insert(book) {
-    let bookTitle = book.title;
-    let bookPages = book.pages
-    if (!bookTitle && !bookPages) { // validation
-      throw new Error('Missing property')
-    }
-    return this.store.insert(book);
-  };
+    // let bookTitle = book.title;
+    // let bookPages = book.pages
+    // if (!bookTitle && !bookPages) { // validation
+    //   throw new Error('Missing property')
+    // }
+    return this.store.insert(book)
+  }
 
   async update(props, book) {
-    return this.store.update(props, book);
+    return this.store.update(props, book)
   }
 
   async remove(props) {
-    return this.store.remove(props);
+    return this.store.remove(props)
   }
 }
 
-export default new BookStore({ filename: './db/books.json', autoload: true });
+export default new BookStore({ filename: './db/books.json', autoload: true })
